aac29145bfd9681edb5cb67927b6e591d42a7aed
Bperez4197 <bryceperez31@gmail.com>
2017-10-31 13:25:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
     count = 0
     board.each do |move|
-      if move == "X" || "O"
+      if move == "X" || move "O"
         count += 1
       end
     end
